48fb3fcc2461dac079d9296f9d155d3f4f00b3ab
Fabrizio De Santis <desantisfabri98@gmail.com>
2025-05-23 14:41:00 +0200

    Create knowledge_base.py

---
+++ knowledge_base.py
@@ -0,0 +1,39 @@
+'''
+This file contains the logical rules used for the LTN training.
+'''
+# SEPSIS KNOWLEDGE BASE
+'''
+R1: Patients with LacticAcid > 2.0 should be admitted to the ICU.
+R2: Patients with Tachypnea, Suspected Infection, and Critical Heart Rate, should be admitted to the ICU.
+R3: Patients with CRP activity followed by IV Antibiotics and with CRP value > 100 should be admitted to the ICU.
+R4: The activity ERTriage should be followed by the activity ERSepsisTriage.
+R5: The activity ERSepsisTriage should be followed by IV Antibiotics.
+R6: The activity ERSepsisTriage should be followed by the activity IV Liquid.
+'''
+# BPI2012 KNOWLEDGE BASE
+'''
+R1: If the Requested Amount is less than 10000, then the application should not be accepted.
+R2: If the Requested Amount is greater than 50000 and less than 100000, then the application should not be accepted.
+R3: If the resources 10910 and 11169 perform an activity, then the application should not be accepted.
+R4:
+R5:
+R6:
+'''
+# BPI2017 KNOWLEDGE BASE
+'''
+R1:
+R2:
+R3:
+R4:
+R5:
+R6:
+'''
+# TRAFFIC FINES KNOWLEDGE BASE
+'''
+R1:
+R2:
+R3:
+R4:
+R5:
+R6:
+'''
