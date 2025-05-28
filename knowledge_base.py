051692d11bd272fde496ac838db9a5cd2e311614
Fabrizio De Santis <desantisfabri98@gmail.com>
2025-05-28 12:27:45 +0200

    Update knowledge_base.py

--- knowledge_base.py
+++ knowledge_base.py
@@ -4,11 +4,11 @@ This file contains the logical rules used for the LTN training.
 # SEPSIS KNOWLEDGE BASE
 '''
 R1: Patients with LacticAcid > 2.0 should be admitted to the ICU. 
-    \/ x (LacticAcid(x) > 2.0 -> ICU(x))
+    \-/ x (LacticAcid(x) > 2.0 -> ICU(x))
 R2: Patients with Tachypnea, Suspected Infection, and Critical Heart Rate, should be admitted to the ICU. 
-    \/ x (Tachypnea(x) /\ SuspectedInfection(x) /\ CriticalHeartRate(x) -> ICU(x))
+    \-/ x (Tachypnea(x) /\ SuspectedInfection(x) /\ CriticalHeartRate(x) -> ICU(x))
 R3: Patients with CRP activity followed by IV Antibiotics and with CRP value > 100 should be admitted to the ICU. 
-    \/ x (CRP(x) /\ IVAntibiotics(x) /\ CRPValue(x) > 100 -> ICU(x))
+    \-/ x (CRP(x) /\ IVAntibiotics(x) /\ CRPValue(x) > 100 -> ICU(x))
 R4: If the activity ERTriage occurs, the activity ERSepsisTriage should occur after ERTriage.
 R5: If the activity ERSepsisTriage occurs, the activity IV Antibiotics should occur after ERSepsisTriage.
 R6: If the activity ERSepsisTriage occurs, the activity IV Liquid should occur after ERSepsisTriage.
@@ -16,11 +16,11 @@ R6: If the activity ERSepsisTriage occurs, the activity IV Liquid should occur a
 # BPI2012 KNOWLEDGE BASE
 '''
 R1: If the Requested Amount is less than 10000, then the application should not be accepted. 
-    \/ x (RequestedAmount(x) < 10000 -> ApplicationAccepted(x))
+    \-/ x (RequestedAmount(x) < 10000 -> ApplicationAccepted(x))
 R2: If the Requested Amount is greater than 50000 and less than 100000, then the application should not be accepted. 
-    \/ x (RequestedAmount(x) > 50000 /\ RequestedAmount(x) < 100000 -> Not(ApplicationAccepted(x)))
+    \-/ x (RequestedAmount(x) > 50000 /\ RequestedAmount(x) < 100000 -> Not(ApplicationAccepted(x)))
 R3: If the resources 10910 and 11169 perform an activity, then the application should not be accepted. 
-    \/ x (HasResource(x,10910) /\ HasResource(x,11169) -> Not(ApplicationAccepted(x)))
+    \-/ x (HasResource(x,10910) /\ HasResource(x,11169) -> Not(ApplicationAccepted(x)))
 R4: If the activity W_Completeren aanvraag-COMPLETE occurs, the activity A_ACCEPTED_COMPLETE should occur after W_Completeren aanvraag-COMPLETE.
 R5: If the activity W_Valideren aanvraag-COMPLETE occurs, the activity O_ACCEPTED_COMPLETE should occur after W_Valideren aanvraag-COMPLETE.
 R6: If the activity O_SENT_BACK_COMPLETEoccurs, the activity W_Valideren aanvraag-COMPLETE should occur after O_SENT_BACK_COMPLETE.
@@ -28,11 +28,11 @@ R6: If the activity O_SENT_BACK_COMPLETEoccurs, the activity W_Valideren aanvraa
 # BPI2017 KNOWLEDGE BASE
 '''
 R1: If the Credit Score is greater than 0 and the Requested Amount is less than 20000, then the application should be accepted. 
-    \/ x (CreditScore(x) > 0 /\ RequestedAmount(x) < 20000 -> ApplicationAccepted(x))
+    \-/ x (CreditScore(x) > 0 /\ RequestedAmount(x) < 20000 -> ApplicationAccepted(x))
 R2: If there is no offer with Credit Score greater than 0, then the application should not be accepted. 
-    \/ x (Not(Offer(x)) /\ CreditScore(x) > 0 -> Not(ApplicationAccepted(x)))
+    \-/ x (Not(Offer(x)) /\ CreditScore(x) > 0 -> Not(ApplicationAccepted(x)))
 R3: If the Requested Amount is greater than 20000 and the Loan Goal is "Existing loan takeover", then the application should be accepted.   
-    \/ x (RequestedAmount(x) > 20000 /\ LoanGoal(x) = "Existing loan takeover" -> ApplicationAccepted(x))
+    \-/ x (RequestedAmount(x) > 20000 /\ LoanGoal(x) = "Existing loan takeover" -> ApplicationAccepted(x))
 R4: If the activity A_SUBMITTED occurs, the activity A_ACCEPTED should occur after A_SUBMITTED.
 R5: If the activity A_ACCEPTED occurs, the activity O_CREATE_OFFER should occur after A_ACCEPTED.
 R6: If the activity A_Complete occurs, the activity W_validate application should occur after A_Complete.
