91f4275d63113fa82b9eca67a5192677869ce13e
Fabrizio De Santis <desantisfabri98@gmail.com>
2025-05-28 22:08:53 +0200

    Update preprocess_bpi12.py

--- data/preprocess_bpi12.py
+++ data/preprocess_bpi12.py
@@ -3,70 +3,12 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 import random
 
-# def create_test_set(data):
-#     grouped = data.groupby("case:concept:name")
-#     unique_groups = list(grouped.groups.keys())  # Get the unique group IDs
-#     labels = data.groupby("case:concept:name")["label"].first().to_list()
-
-#     len_test_set = int(len(unique_groups) * 0.3)
-
-#     labels_r1 = data.groupby("case:concept:name")["rule_1"].first().to_list()
-#     labels_r2 = data.groupby("case:concept:name")["rule_2"].first().to_list()
-#     labels_r3 = data.groupby("case:concept:name")["rule_3"].first().to_list()
-
-#     filtered_values_r1 = [v for v, l, r in zip(unique_groups, labels, labels_r1) if (r == 1 and l == 0)]
-#     filtered_values_r2 = [v for v, l, r in zip(unique_groups, labels, labels_r2) if (r == 1 and l == 0)]
-#     filtered_values_r3 = [v for v, l, r in zip(unique_groups, labels, labels_r3) if (r == 1 and l == 0)]
-
-#     filtered_values_nor = [v for v, l, r1, r2, r3 in zip(unique_groups, labels, labels_r1, labels_r2, labels_r3) if (r1 == 0 and r2 == 0 and r3 == 0 and l == 1)]
-
-#     filtered_values_r1_training = random.sample(filtered_values_r1, len(filtered_values_r1) // 3)
-#     filtered_values_r1_test = [x for x in filtered_values_r1 if x not in filtered_values_r1_training]
-#     print("Len compliant traces in training set: ", len(filtered_values_r1_training))
-#     print("Len compliant traces in test set: ", len(filtered_values_r1_test))
-#     filtered_values_r2_training = random.sample(filtered_values_r2, len(filtered_values_r2) // 3)
-#     filtered_values_r2_test = [x for x in filtered_values_r2 if x not in filtered_values_r2_training]
-#     print("Len compliant traces in training set: ", len(filtered_values_r2_training))
-#     print("Len compliant traces in test set: ", len(filtered_values_r2_test))
-#     filtered_values_r3_training = random.sample(filtered_values_r3, len(filtered_values_r3) // 3)
-#     filtered_values_r3_test = [x for x in filtered_values_r3 if x not in filtered_values_r3_training]
-#     print("Len compliant traces in training set: ", len(filtered_values_r3_training))
-#     print("Len compliant traces in test set: ", len(filtered_values_r3))
-
-#     print("Number of compliant rules in the training set: ", len(list(set(filtered_values_r1_training + filtered_values_r2_training + filtered_values_r3_training))))
-
-#     filtered_values = filtered_values_r1_test + filtered_values_r2_test + filtered_values_r3_test + filtered_values_nor
-#     compliant_ids = list(set(filtered_values_r1 + filtered_values_r2 + filtered_values_r3))
-#     filtered_values = list(set(filtered_values))  # Remove duplicates
-
-#     print("Len filtered values: ", len(filtered_values))
-#     # take len_test_set from filtered_values
-#     if len(filtered_values) > len_test_set:
-#           # Set seed for reproducibility
-#         test_ids = random.sample(filtered_values, len_test_set)
-#     else:
-#         test_ids = filtered_values
-
-#     # Remove the selected test IDs from the original list
-#     training_ids = [x for x in unique_groups if x not in test_ids]
-#     #training_ids = training_ids + filtered_values_r1_training + filtered_values_r2_training + filtered_values_r3_training
-#     training_ids = list(set(training_ids))  # Remove duplicates
-
-#     compliant_training_ids = [x for x in training_ids if x in compliant_ids]
-#     compliant_test_ids = [x for x in test_ids if x in compliant_ids]
-#     print("Number of compliant traces in training set: ", len(compliant_training_ids))
-#     print("Number of compliant traces in test set: ", len(compliant_test_ids))
-
-#     return training_ids, test_ids
-
 def create_test_set(data):
-    random.seed(42)
     grouped = data.groupby("case:concept:name")
     unique_groups = list(grouped.groups.keys())  # Get the unique group IDs
     labels = data.groupby("case:concept:name")["label"].first().to_list()
 
     len_test_set = int(len(unique_groups) * 0.3)
-    len_training_set = len(unique_groups) - len_test_set
 
     labels_r1 = data.groupby("case:concept:name")["rule_1"].first().to_list()
     labels_r2 = data.groupby("case:concept:name")["rule_2"].first().to_list()
@@ -75,34 +17,93 @@ def create_test_set(data):
     filtered_values_r1 = [v for v, l, r in zip(unique_groups, labels, labels_r1) if (r == 1 and l == 0)]
     filtered_values_r2 = [v for v, l, r in zip(unique_groups, labels, labels_r2) if (r == 1 and l == 0)]
     filtered_values_r3 = [v for v, l, r in zip(unique_groups, labels, labels_r3) if (r == 1 and l == 0)]
+
+    filtered_values_nor = [v for v, l, r1, r2, r3 in zip(unique_groups, labels, labels_r1, labels_r2, labels_r3) if (r1 == 0 and r2 == 0 and r3 == 0 and l == 1)]
+
+    # filtered_values_r1_training = random.sample(filtered_values_r1, len(filtered_values_r1) // 2)
+    # filtered_values_r1_test = [x for x in filtered_values_r1 if x not in filtered_values_r1_training]
+    # print("Len compliant traces in training set: ", len(filtered_values_r1_training))
+    # print("Len compliant traces in test set: ", len(filtered_values_r1_test))
+    # filtered_values_r2_training = random.sample(filtered_values_r2, len(filtered_values_r2) // 2)
+    # filtered_values_r2_test = [x for x in filtered_values_r2 if x not in filtered_values_r2_training]
+    # print("Len compliant traces in training set: ", len(filtered_values_r2_training))
+    # print("Len compliant traces in test set: ", len(filtered_values_r2_test))
+    # filtered_values_r3_training = random.sample(filtered_values_r3, len(filtered_values_r3) // 2)
+    # filtered_values_r3_test = [x for x in filtered_values_r3 if x not in filtered_values_r3_training]
+    # print("Len compliant traces in training set: ", len(filtered_values_r3_training))
+    # print("Len compliant traces in test set: ", len(filtered_values_r3))
+
+    #print("Number of compliant rules in the training set: ", len(list(set(filtered_values_r1_training + filtered_values_r2_training + filtered_values_r3_training))))
+
+    filtered_values = filtered_values_r1 + filtered_values_r2 + filtered_values_r3 + filtered_values_nor
+    # filtered_values = filtered_values_r1_test + filtered_values_r2_test + filtered_values_r3_test + filtered_values_nor
     compliant_ids = list(set(filtered_values_r1 + filtered_values_r2 + filtered_values_r3))
+    filtered_values = list(set(filtered_values))  # Remove duplicates
 
-    filtered_no_r1 = [v for v, l, r in zip(unique_groups, labels, labels_r1) if (r == 1 and l != 0)]
-    filtered_no_r2 = [v for v, l, r in zip(unique_groups, labels, labels_r2) if (r == 1 and l != 0)]
-    filtered_no_r3 = [v for v, l, r in zip(unique_groups, labels, labels_r3) if (r == 1 and l != 0)]
-    non_compliant_ids = list(set(filtered_no_r1 + filtered_no_r2 + filtered_no_r3))
-
-    print("Len training set: ", len_training_set)
-    print("Len test set: ", len_test_set)
-    print("Number of compliant traces: ", len(compliant_ids))
-    print("Number of non-compliant traces: ", len(non_compliant_ids))
-
-    if len(compliant_ids) > len_test_set:
-        test_ids = random.sample(compliant_ids, int(len_test_set*0.7))
-        # fill the rest with ids from unique_groups that are not in non_compliant_ids
-        remaining_ids = [x for x in unique_groups if x not in test_ids and x not in non_compliant_ids]
-        remaining_ids = random.sample(remaining_ids, len_test_set - len(test_ids))
-        test_ids = test_ids + remaining_ids
+    print("Len filtered values: ", len(filtered_values))
+    # take len_test_set from filtered_values
+    if len(filtered_values) > len_test_set:
+          # Set seed for reproducibility
+        test_ids = random.sample(filtered_values, len_test_set)
     else:
-        test_ids = compliant_ids
+        test_ids = filtered_values
 
+    # Remove the selected test IDs from the original list
     training_ids = [x for x in unique_groups if x not in test_ids]
+    #training_ids = training_ids + filtered_values_r1_training + filtered_values_r2_training + filtered_values_r3_training
+    training_ids = list(set(training_ids))  # Remove duplicates
 
-    #### 
-    print("Compliant traces in training set: ", len([x for x in training_ids if x in compliant_ids]))
-    print("Compliant traces in test set: ", len([x for x in test_ids if x in compliant_ids]))
+    compliant_training_ids = [x for x in training_ids if x in compliant_ids]
+    compliant_test_ids = [x for x in test_ids if x in compliant_ids]
+    print("Number of compliant traces in training set: ", len(compliant_training_ids))
+    print("Number of compliant traces in test set: ", len(compliant_test_ids))
 
     return training_ids, test_ids
+
+# def create_test_set(data):
+#     random.seed(42)
+#     grouped = data.groupby("case:concept:name")
+#     unique_groups = list(grouped.groups.keys())  # Get the unique group IDs
+#     labels = data.groupby("case:concept:name")["label"].first().to_list()
+
+#     len_test_set = int(len(unique_groups) * 0.2)
+#     len_training_set = len(unique_groups) - len_test_set
+
+#     labels_r1 = data.groupby("case:concept:name")["rule_1"].first().to_list()
+#     labels_r2 = data.groupby("case:concept:name")["rule_2"].first().to_list()
+#     labels_r3 = data.groupby("case:concept:name")["rule_3"].first().to_list()
+
+#     filtered_values_r1 = [v for v, l, r in zip(unique_groups, labels, labels_r1) if (r == 1 and l == 0)]
+#     filtered_values_r2 = [v for v, l, r in zip(unique_groups, labels, labels_r2) if (r == 1 and l == 0)]
+#     filtered_values_r3 = [v for v, l, r in zip(unique_groups, labels, labels_r3) if (r == 1 and l == 0)]
+#     compliant_ids = list(set(filtered_values_r1 + filtered_values_r2 + filtered_values_r3))
+
+#     filtered_no_r1 = [v for v, l, r in zip(unique_groups, labels, labels_r1) if (r == 1 and l != 0)]
+#     filtered_no_r2 = [v for v, l, r in zip(unique_groups, labels, labels_r2) if (r == 1 and l != 0)]
+#     filtered_no_r3 = [v for v, l, r in zip(unique_groups, labels, labels_r3) if (r == 1 and l != 0)]
+#     non_compliant_ids = list(set(filtered_no_r1 + filtered_no_r2 + filtered_no_r3))
+
+#     print("Len training set: ", len_training_set)
+#     print("Len test set: ", len_test_set)
+#     print("Number of compliant traces: ", len(compliant_ids))
+#     print("Number of non-compliant traces: ", len(non_compliant_ids))
+
+#     if len(compliant_ids) > len_test_set:
+#         test_ids = random.sample(compliant_ids, int(len_test_set*0.7))
+#         # fill the rest with ids from unique_groups that are not in non_compliant_ids
+#         remaining_ids = [x for x in unique_groups if x not in test_ids and x not in non_compliant_ids]
+#         remaining_ids = random.sample(remaining_ids, len_test_set - len(test_ids))
+#         test_ids = test_ids + remaining_ids
+#     else:
+#         test_ids = compliant_ids
+
+#     training_ids = [x for x in unique_groups if x not in test_ids]
+
+#     #### 
+#     print("Compliant traces in training set: ", len([x for x in training_ids if x in compliant_ids]))
+#     print("Compliant traces in test set: ", len([x for x in test_ids if x in compliant_ids]))
+
+#     return training_ids, test_ids
 
 def create_ngrams(data, train_ids, test_ids, window_size=40):
 
