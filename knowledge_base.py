0579135ad87843ddcd29fbdd8c99cf25a1c5432c
Fabrizio De Santis <desantisfabri98@gmail.com>
2025-05-28 11:34:16 +0200

    Update knowledge_base.py

--- knowledge_base.py
+++ knowledge_base.py
@@ -9,9 +9,9 @@ R2: Patients with Tachypnea, Suspected Infection, and Critical Heart Rate, shoul
     \/ x (Tachypnea(x) /\ SuspectedInfection(x) /\ CriticalHeartRate(x) -> ICU(x))
 R3: Patients with CRP activity followed by IV Antibiotics and with CRP value > 100 should be admitted to the ICU. 
     \/ x (CRP(x) /\ IVAntibiotics(x) /\ CRPValue(x) > 100 -> ICU(x))
-R4: The activity ERTriage should be followed by the activity ERSepsisTriage.
-R5: The activity ERSepsisTriage should be followed by IV Antibiotics.
-R6: The activity ERSepsisTriage should be followed by the activity IV Liquid.
+R4: If the activity ERTriage occurs, the activity ERSepsisTriage should occur after ERTriage.
+R5: If the activity ERSepsisTriage occurs, the activity IV Antibiotics should occur after ERSepsisTriage.
+R6: If the activity ERSepsisTriage occurs, the activity IV Liquid should occur after ERSepsisTriage.
 '''
 # BPI2012 KNOWLEDGE BASE
 '''
@@ -21,9 +21,9 @@ R2: If the Requested Amount is greater than 50000 and less than 100000, then the
     \/ x (RequestedAmount(x) > 50000 /\ RequestedAmount(x) < 100000 -> Not(ApplicationAccepted(x)))
 R3: If the resources 10910 and 11169 perform an activity, then the application should not be accepted. 
     \/ x (HasResource(x,10910) /\ HasResource(x,11169) -> Not(ApplicationAccepted(x)))
-R4: The activity W_Completeren aanvraag-COMPLETE should be followed by the activity A_ACCEPTED_COMPLETE
-R5: The activity W_Valideren aanvraag-COMPLETE should be followed by the activity O_ACCEPTED_COMPLETE
-R6: The activity O_SENT_BACK_COMPLETE should be followed by the activity W_Valideren aanvraag-COMPLETE
+R4: If the activity W_Completeren aanvraag-COMPLETE occurs, the activity A_ACCEPTED_COMPLETE should occur after W_Completeren aanvraag-COMPLETE.
+R5: If the activity W_Valideren aanvraag-COMPLETE occurs, the activity O_ACCEPTED_COMPLETE should occur after W_Valideren aanvraag-COMPLETE.
+R6: If the activity O_SENT_BACK_COMPLETEoccurs, the activity W_Valideren aanvraag-COMPLETE should occur after O_SENT_BACK_COMPLETE.
 '''
 # BPI2017 KNOWLEDGE BASE
 '''
@@ -33,9 +33,9 @@ R2: If there is no offer with Credit Score greater than 0, then the application 
     \/ x (Not(Offer(x)) /\ CreditScore(x) > 0 -> Not(ApplicationAccepted(x)))
 R3: If the Requested Amount is greater than 20000 and the Loan Goal is "Existing loan takeover", then the application should be accepted.   
     \/ x (RequestedAmount(x) > 20000 /\ LoanGoal(x) = "Existing loan takeover" -> ApplicationAccepted(x))
-R4: The activity A_SUBMITTED should be followed by the activity A_ACCEPTED.
-R5: The activity A_ACCEPTED should be followed by the activity O_CREATE_OFFER.
-R6: The activity A_Complete should be followed by the activity W_validate application.
+R4: If the activity A_SUBMITTED occurs, the activity A_ACCEPTED should occur after A_SUBMITTED.
+R5: If the activity A_ACCEPTED occurs, the activity O_CREATE_OFFER should occur after A_ACCEPTED.
+R6: If the activity A_Complete occurs, the activity W_validate application should occur after A_Complete.
 '''
 # TRAFFIC FINES KNOWLEDGE BASE
 '''
@@ -45,7 +45,7 @@ R2: If the activity Add penalty occurs, then the fine should be sent for credit 
     \/x (HasActivity(x, "Add penalty") -> SendToCreditCollection(x))
 R3: If the amount of the fine is greater than 400, then the fine should be sent for credit collection. 
     \/x (FineAmount(x) > 400 -> SendToCreditCollection(x))
-R4: The activity Create Fine should be followed by the activity Send Fine.
-R5: The activity Send Fine should be followed by the activity Insert Fine Notification.
-R6: The activity Send Fine should be followed by the activity Payment.
+R4: If the activity Create Fine occurs, the activity Send Fine should occur after Create Fine.
+R5: If the activity Send Fine occurs, the activity Insert Fine Notification should occur after Send Fine.
+R6: If the activity Send Fine occurs, the activity Payment Notification should occur after Send Fine.
 '''
